bce6b660223d46fe4364290185b3f51a95929278
JulioCesarPP <julio798cesarpp@gmail.com>
2024-10-23 19:11:04 -0600

    Bloc de notas en Kotlin

---
+++ app/src/main/java/mx/edu/dsi_code/notasmvvm/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package mx.edu.dsi_code.notasmvvm.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purpura20 = Color(0xFFC4E2A2)
+val Purple80 = Color(0xFFC4E2A2)
+val PurpleGrey80 = Color(0xFFC4E2A2)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF66AF69)
+val PurpleGrey40 = Color(0xFFC8E6A7)
+val Pink40 = Color(0xFF7D5260)
+
+val SuPlazaPrimaryColor = Color(0xFFE6620F)
